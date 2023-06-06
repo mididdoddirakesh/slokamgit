857df5bf0dec77ce6252ada7f84b74bc02ed011d
rakeshveeru <rakeshveeru319@gmail.com>
2023-06-06 00:43:33 -0700

    eclipegit projec

---
+++ src/com/slokam/pojo.java
@@ -0,0 +1,48 @@
+package com.slokam;
+
+public class pojo {
+	
+	
+	public class adharPojo {
+
+		private int aid;
+		private String anum;
+		private String aloc;
+		private int aage;
+		public int getAid() {
+			return aid;
+		}
+		public void setAid(int aid) {
+			this.aid = aid;
+		}
+		public String getAnum() {
+			return anum;
+		}
+		public void setAnum(String anum) {
+			this.anum = anum;
+		}
+		public String getAloc() {
+			return aloc;
+		}
+		public void setAloc(String aloc) {
+			this.aloc = aloc;
+		}
+		public int getAage() {
+			return aage;
+		}
+		public void setAage(int aage) {
+			this.aage = aage;
+		}
+		@Override
+		public String toString() {
+			return "adharPojo [aid=" + aid + ", anum=" + anum + ", aloc=" + aloc + ", aage=" + aage + "]";
+		}
+		
+		
+	
+		
+		
+		
+	}
+		}
+		
