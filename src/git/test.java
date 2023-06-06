faddf099328f6adb6e14248c445614a5bf4903cc
rakeshveeru <rakeshveeru319@gmail.com>
2023-06-06 00:29:43 -0700

    eclipsegit practice

---
+++ src/git/test.java
@@ -0,0 +1,22 @@
+package git;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test {
+	
+	public static void m1()
+	{
+		System.out.println("java developer");
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		
+	m1();
+	
+		 
+	}
+
+}
